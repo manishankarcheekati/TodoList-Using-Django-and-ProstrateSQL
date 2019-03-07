2c79439d51c43ca99b1e56fc234ba8926f3b3197
manishankarcheekati <manishankar.cheekati@gmail.com>
2019-03-07 14:13:46 +0530

    TodoList
    A simple Todo List Using Django an ProstrateSQL

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
 from . import models
 
 class TodoListAdmin(admin.ModelAdmin):
--- todolist/models.py
+++ todolist/models.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
-
-# Create your models here.
-class Category(models.Model): # The Category table name that inherits models.Model
+class Category(models.Model): 
 	name = models.CharField(max_length=100) #Like a varchar
 
 	class Meta:
@@ -13,17 +10,18 @@ class Category(models.Model): # The Category table name that inherits models.Mod
 		verbose_name_plural = ("Categories")
 
 	def __str__(self):
-		return self.name #name to be shown when called
+		return self.name 
 
-class TodoList(models.Model): #Todolist able name that inherits models.Model
-	title = models.CharField(max_length=250) # a varchar
-	content = models.TextField(blank=True) # a text field 
-	created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
-	due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
-	category = models.ForeignKey(Category, default="general") # a foreignkey
+class TodoList(models.Model): 
+	title = models.CharField(max_length=250)
+	content = models.TextField(blank=True) 
+	created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+	due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+	category = models.ForeignKey(Category, default="general") 
 
 	class Meta:
-		ordering = ["-created"] #ordering by the created field
+		ordering = ["-created"] 
 
 	def __str__(self):
-		return self.title #name to be shown when called
+		return self.title
+	
